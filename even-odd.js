e275e489bb223f0b65485efe2604688f8511e3dc
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-04 23:18:44 +0600

    use of if else in a function

--- even-odd.js
+++ even-odd.js
@@ -1,6 +1,12 @@
 //Shortcut way to see reminder
 function isEven(number) {
     const reminder = number % 2;
-    console.log(reminder);
+    // console.log(reminder);
+    if (reminder == 0) {
+        console.log('The Number is Even: ');
+    }
+    else {
+        console.log('The number is odd: ');
+    }
 }
 isEven(550);
