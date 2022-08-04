c9a4d7ade591723bb5fb73edb1030c7073268e5f
shubhoahmed <shubhoahmed03@gmail.com>
2022-08-05 00:15:59 +0600

    logical leap year check

--- leap-year.js
+++ leap-year.js
@@ -1,10 +1,22 @@
 //Leap year check program
+// function isLeapYear(year) {
+//     const reminder = year % 4;
+//     if (reminder === 0) {
+//         return true;
+//     }
+//     return false;
+// }
+// const giveYearInput = 2024;
+// const checkLeapYear = isLeapYear(giveYearInput);
+// console.log(checkLeapYear);
+
 function isLeapYear(year) {
     const reminder = year % 4;
-    if (reminder === 0) {
+    if ((reminder === 0) && (reminder != year % 100)(reminder === year % 400)) {
         return true;
     }
     return false;
 }
-const giveYearInput = 2021;
+const giveYearInput = 2027;
 const checkLeapYear = isLeapYear(giveYearInput);
+console.log(checkLeapYear);
